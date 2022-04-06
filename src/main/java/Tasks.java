b213b8643fe719c095b25579800a2cfc3dc60419
Alex <abolduev@yandex.ru>
2022-04-06 18:02:53 +0300

    First commit

---
+++ src/main/java/Tasks.java
@@ -0,0 +1,23 @@
+public class Tasks {
+    String record = "";
+
+    public Tasks(String record) {
+        this.record = record;
+    }
+
+    public Tasks() {
+    }
+
+    public String getRecord() {
+        return record;
+    }
+
+    public void setRecord(String record) {
+        this.record = record;
+    }
+
+    public void add(){
+        record =
+
+    }
+}
