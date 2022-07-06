5e6285487e3595f4920b8f699351d97560f33413
Alex <abolduev@yandex.ru>
2022-07-06 11:30:56 +0300

    Rest api

--- src/main/java/task/Main.java
+++ src/main/java/task/Main.java
@@ -7,12 +7,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.web.bind.annotation.RestController;
 import task.methods.ActionExecutor;
 import java.util.List;
 import java.util.Scanner;
 
 @SpringBootApplication
 @RequiredArgsConstructor
+@RestController
 @EntityScan(basePackages = {"task"})
 public class Main implements CommandLineRunner {
     static TaskList taskList = TaskList.getInstance();
